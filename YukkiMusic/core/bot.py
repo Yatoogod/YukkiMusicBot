05fb5f2eaef77a7a70a1f0b8dc0d8fdf21d146f3
Yatoogod <email.yamann@gmail.com>
2024-11-15 23:00:50 +0530

    Update bot.py

--- YukkiMusic/core/bot.py
+++ YukkiMusic/core/bot.py
@@ -1,19 +1,9 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-
 import sys
-
+import asyncio
 from pyrogram import Client
 from pyrogram.types import BotCommand
-
+from pyrogram.errors import BadMsgNotification
 import config
-
 from ..logging import LOGGER
 
 
@@ -28,46 +18,50 @@ class YukkiBot(Client):
         )
 
     async def start(self):
-        await super().start()
-        get_me = await self.get_me()
-        self.username = get_me.username
-        self.id = get_me.id
-        try:
-            await self.send_message(
-                config.LOG_GROUP_ID, "Bot Started"
-            )
-        except:
-            LOGGER(__name__).error(
-                "Bot has failed to access the log Group. Make sure that you have added your bot to your log channel and promoted as admin!"
-            )
-            sys.exit()
-        if config.SET_CMDS == str(True):
+        while True:
             try:
-                await self.set_bot_commands(
-                    [
-                        BotCommand("ping", "Check that bot is alive or dead"),
-                        BotCommand("play", "Starts playing the requested song"),
-                        BotCommand("skip", "Moves to the next track in queue"),
-                        BotCommand("pause", "Pause the current playing song"),
-                        BotCommand("resume", "Resume the paused song"),
-                        BotCommand("end", "Clear the queue and leave voice chat"),
-                        BotCommand("shuffle", "Randomly shuffles the queued playlist."),
-                        BotCommand("playmode", "Allows you to change the default playmode for your chat"),
-                        BotCommand("settings", "Open the settings of the music bot for your chat.")
-                        ]
+                await super().start()
+                get_me = await self.get_me()
+                self.username = get_me.username
+                self.id = get_me.id
+                try:
+                    await self.send_message(config.LOG_GROUP_ID, "Bot Started")
+                except:
+                    LOGGER(__name__).error(
+                        "Bot has failed to access the log Group. Make sure that you have added your bot to your log channel and promoted as admin!"
                     )
-            except:
-                pass
-        else:
-            pass
-        a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
-        if a.status != "administrator":
-            LOGGER(__name__).error(
-                "Please promote Bot as Admin in Logger Group"
-            )
-            sys.exit()
-        if get_me.last_name:
-            self.name = get_me.first_name + " " + get_me.last_name
-        else:
-            self.name = get_me.first_name
-        LOGGER(__name__).info(f"MusicBot Started as {self.name}")
+                    sys.exit()
+                if config.SET_CMDS == str(True):
+                    try:
+                        await self.set_bot_commands(
+                            [
+                                BotCommand("ping", "Check that bot is alive or dead"),
+                                BotCommand("play", "Starts playing the requested song"),
+                                BotCommand("skip", "Moves to the next track in queue"),
+                                BotCommand("pause", "Pause the current playing song"),
+                                BotCommand("resume", "Resume the paused song"),
+                                BotCommand("end", "Clear the queue and leave voice chat"),
+                                BotCommand("shuffle", "Randomly shuffles the queued playlist."),
+                                BotCommand("playmode", "Allows you to change the default playmode for your chat"),
+                                BotCommand("settings", "Open the settings of the music bot for your chat.")
+                            ]
+                        )
+                    except:
+                        pass
+                else:
+                    pass
+                a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
+                if a.status != "administrator":
+                    LOGGER(__name__).error(
+                        "Please promote Bot as Admin in Logger Group"
+                    )
+                    sys.exit()
+                if get_me.last_name:
+                    self.name = get_me.first_name + " " + get_me.last_name
+                else:
+                    self.name = get_me.first_name
+                LOGGER(__name__).info(f"MusicBot Started as {self.name}")
+                break  # Exit the loop when everything is successful
+            except BadMsgNotification:
+                LOGGER(__name__).warning("Time synchronization issue detected. Retrying in 5 seconds...")
+                await asyncio.sleep(5)  # Retry after 5 seconds
